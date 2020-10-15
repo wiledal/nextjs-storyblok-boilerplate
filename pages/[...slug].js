fc7edefb260f9c98c7d4a0beadc4fdd980cc5538
Hugo Wiledal <hugo.wiledal@gmail.com>
2020-10-15 20:19:19 +0200

    Simplified structure

---
+++ pages/[...slug].js
@@ -0,0 +1,45 @@
+import ModuleDelegator from "../components/common/module-delegator";
+import useEditableContent from "../hooks/use-editable-content";
+import SbEditable from "storyblok-react";
+import Layout from "../components/layout";
+import { withDefaultProps } from "../hofs/with-default-props";
+import { useRouter } from "next/router";
+import LoadingSpinner from "../components/common/loading-spinner";
+import Error from "../components/common/error";
+
+export default function AnySlug(props) {
+  let { preview, page, settings } = props;
+
+  let content = useEditableContent(page);
+  let router = useRouter();
+
+  return (
+    <>
+      <Layout {...props}>
+        {content && (
+          <SbEditable content={content} key={content?._uid}>
+            <div className="page">
+              <ModuleDelegator modules={content?.body} />
+            </div>
+          </SbEditable>
+        )}
+        {!content && <Error />}
+      </Layout>
+
+      <LoadingSpinner show={router.isFallback} />
+    </>
+  );
+}
+
+export const getStaticProps = withDefaultProps((ctx) => {
+  return {
+    props: {},
+  };
+});
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,4 @@
-import ModuleDelegator from "../components/common/module-delegator";
-import useEditableContent from "../hooks/use-editable-content";
-import SbEditable from "storyblok-react";
-import Layout from "../components/layout";
-import { withDefaultProps } from "../hofs/with-default-props";
-import { useRouter } from "next/router";
-import LoadingSpinner from "../components/common/loading-spinner";
-import Error from "../components/common/error";
+import Index, { getStaticProps } from "./[...slug]";
 
-export default function Index(props) {
-  let { preview, page, settings } = props;
-
-  let content = useEditableContent(page);
-  let router = useRouter();
-
-  return (
-    <>
-      <Layout {...props}>
-        {content && (
-          <SbEditable content={content} key={content?._uid}>
-            <div className="page">
-              <ModuleDelegator modules={content?.body} />
-            </div>
-          </SbEditable>
-        )}
-        {!content && <Error />}
-      </Layout>
-
-      <LoadingSpinner show={router.isFallback} />
-    </>
-  );
-}
-
-export const getStaticProps = withDefaultProps((ctx) => {
-  return {
-    props: {},
-  };
-});
+export default Index;
+export { getStaticProps };
